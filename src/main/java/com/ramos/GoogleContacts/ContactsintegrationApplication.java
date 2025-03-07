083b142eb24d7633c61388b15b6f18546b7ec86f
Raveneko <168397732+Raveneko@users.noreply.github.com>
2025-03-07 23:32:15 +0800

    2nd commit

--- src/main/java/com/ramos/GoogleContacts/ContactsintegrationApplication.java
+++ src/main/java/com/ramos/GoogleContacts/ContactsintegrationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoogleContactsIntegrationApplication {
+public class ContactsintegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoogleContactsIntegrationApplication.class, args);
+		SpringApplication.run(ContactsintegrationApplication.class, args);
 	}
 
 }
